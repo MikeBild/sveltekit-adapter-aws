0535921fe51a4ca6e1c8fcaddd32dcc03ded4a4f
Mike Bild <mike@mikebild.com>
2022-11-10 17:28:22 +0100

    code format

--- lib/adapter-stack.ts
+++ lib/adapter-stack.ts
@@ -18,13 +18,13 @@ import {
 import { CorsHttpMethod, HttpApi, IHttpApi, PayloadFormatVersion } from '@aws-cdk/aws-apigatewayv2-alpha';
 import { HttpLambdaIntegration } from '@aws-cdk/aws-apigatewayv2-integrations-alpha';
 import { config } from 'dotenv';
-import {PolicyStatement} from "aws-cdk-lib/aws-iam";
+import { PolicyStatement } from 'aws-cdk-lib/aws-iam';
 
 export interface AWSAdapterStackProps extends StackProps {
   FQDN: string;
   account?: string;
   region?: string;
-  serverHandlerPolicies?: PolicyStatement[]
+  serverHandlerPolicies?: PolicyStatement[];
 }
 
 export class AWSAdapterStack extends Stack {
@@ -60,7 +60,7 @@ export class AWSAdapterStack extends Stack {
       } as any,
     });
 
-    props.serverHandlerPolicies?.forEach(this.serverHandler.addToRolePolicy)
+    props.serverHandlerPolicies?.forEach(this.serverHandler.addToRolePolicy);
 
     this.httpApi = new HttpApi(this, 'API', {
       corsPreflight: {
